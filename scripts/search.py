8d7a3377123b3812d702e767ec07f804d60207be
Ralph Embree <ralph.embree@brominator.org>
2016-09-30 15:23:05 -0400

    Speed up Wikipedia search by ditching wikipedia API

--- scripts/search.py
+++ scripts/search.py
@@ -2,8 +2,10 @@ import csv
 import html
 from io import StringIO
 
+from bs4 import BeautifulSoup
+import requests
 from pws import Bing
-import wikipedia
+from urllib.parse import quote_plus
 
 def on_search(event, room, client):
     search = Bing.search(event.query)
@@ -17,8 +19,15 @@ def on_search(event, room, client):
     event.respond("\n\n\n".join(messages), False)
 
 def on_wiki(event, room, client):
-    result = wikipedia.page(event.query)
-    event.respond(result.url)
+    url = "https://en.wikipedia.org/w/index.php?search={}".format(quote_plus(event.query))
+    r = requests.get(url)
+    if r.url.startswith("https://en.wikipedia.org/wiki"):
+        event.respond(r.url)
+    else:
+        page = BeautifulSoup(r.text)
+        first_result = page.find("div", {'class': 'mw-search-result-heading'})
+        link = first_result.find('a')
+        event.respond('https://en.wikipedia.org{}'.format(link['href']))
 
 commands = {'search': on_search, 'wiki': on_wiki}
 help = {
