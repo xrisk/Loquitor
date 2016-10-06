a06459c6369f13a8577753532cfdfb988e32a46c
Ralph Embree <ralph.embree@brominator.org>
2016-10-05 21:53:43 -0400

    Add script to welcome new users to the room

---
+++ scripts/new_user.py
@@ -0,0 +1,41 @@
+from random import choice
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+GREETINGS = (
+    "Hey, {}. How are you?", "Hello, {}.", "Heyo there, {}!", "{}: Hello.",
+    "Howdy, {}.",
+)
+
+def greet(room, user_name):
+    greeting = choice(GREETINGS)
+    room.send_message(greeting.format("@" + user_name))
+
+def user_entered(event, room, client):
+    me = client.get_me().id
+    user_id = event.data['user_id']
+    if user_id == me:
+        return
+    room_id = room.room_id
+
+    user = urlopen("https://chat.stackoverflow.com/users/{}".format(user_id))
+    soup = BeautifulSoup(user.read())
+    room_card = soup.find('div', {'class': 'roomcard', 'id': 'room-{}'.format(room_id)})
+    if room_card is None:
+        return
+    messages_tag = room_card.find('div', {'class': 'room-message-count'})
+    message_text = messages_tag['title']
+    message_count = int(message_text.partition(' ')[0])
+
+    if not message_count:
+        greet(room, event.data['user_name'])
+
+def on_greet(event, room, client, bot):
+    for user_name in event.args:
+        user_name = "".join(user_name.split()).strip(",")
+        greet(room, user_name)
+
+def main(room, bot, client):
+    room.connect("user-entered", user_entered)
+    bot.register("greet", on_greet)
