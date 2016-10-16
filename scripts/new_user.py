e44ccce2a80254a6277e4cc3d2c871583e628720
Ralph Embree <ralph.embree@brominator.org>
2016-10-15 22:50:56 -0400

    Remove automatic greeting of newcomers

--- scripts/new_user.py
+++ scripts/new_user.py
@@ -18,33 +18,13 @@ def greet(room, user_name):
     greeting = " ".join([choice(GREETINGS), additional])
     room.send_message(greeting.format("@" + user_name))
 
-def user_entered(event, room, client):
-    me = client.get_me().id
-    user_id = event.data['user_id']
-    if user_id == me or client.get_user(user_id).reputation < 20:
-        return
-    room_id = room.room_id
-
-    user = urlopen("https://chat.stackoverflow.com/users/{}".format(user_id))
-    soup = BeautifulSoup(user.read())
-    room_card = soup.find('div', {'class': 'roomcard', 'id': 'room-{}'.format(room_id)})
-    if room_card is None:
-        return
-    messages_tag = room_card.find('div', {'class': 'room-message-count'})
-    message_text = messages_tag['title']
-    message_count = int(message_text.partition(' ')[0])
-
-    if not message_count:
-        greet(room, event.data['user_name'])
 
 def on_greet(event, room, client, bot):
     for user_name in event.args:
         user_name = user_name.strip(",")
         greet(room, user_name)
 
-def main(room, bot, client):
-    room.connect("user-entered", user_entered)
-    bot.register(
-        "greet", on_greet,
-        help="Given a space-separated list of usernames, greet those users."
-    )
+commands = {"greet": on_greet}
+help = {
+    'greet': "Given a space-separated list of usernames, greet those users."
+}
