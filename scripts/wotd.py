3f8a183c337255b914f137c65f7ef7b7ffe4c949
Ralph Embree <ralph.embree@brominator.org>
2016-11-06 07:47:06 -0500

    Fix bug in wotd so that long messages are displayed; add fun fact to wotd

--- scripts/wotd.py
+++ scripts/wotd.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import date
 from urllib.request import urlopen
 
 from bs4 import BeautifulSoup
@@ -11,22 +11,22 @@ class WOTD:
         self.definitions = []
 
     def __iter__(self):
-        utcnow = datetime.utcnow().date()
-        if self.wotd is None or self.wotd[-1] != utcnow:
+        today = date.today()
+        if self.wotd is None or self.wotd[-1] != today:
             generator = self.get()
-            word, link = next(generator)
-            self.wotd = (word, link, utcnow)
-            yield word, link
+            info = next(generator)
+            self.wotd = info + (today,)
+            yield info
             self.definitions[:] = []
             for definition in generator:
                 self.definitions.append(definition)
                 yield definition
         else:
-            yield self.wotd[:2]
+            yield self.wotd[:-1]
             yield from self.definitions
 
     def on_wotd(self, event, room, client, bot):
-        event.message.reply(self.format())
+        event.message.reply(self.format(), False)
 
     def get(self):
         feed = feedparser.parse("https://en.wiktionary.org/w/api.php?action=featuredfeed&feed=wotd")
@@ -34,20 +34,25 @@ class WOTD:
 
         span = soup.find('span', {'id': 'WOTD-rss-title'})
         word = span.text
+        speech_part = soup.find('i').text
         link = 'https://en.wiktionary.org' + span.parent['href']
+        fun_fact_tag = soup.find_all('small')[-1]
+        fun_fact = fun_fact_tag.text
+        fun_fact_tag.clear()
 
-        yield word, link
+        yield word, speech_part, link, fun_fact
 
         definitions = soup.find("ol")
         yield from (definition.text for definition in definitions.find_all("li"))
 
     def format(self):
         wotd_generator = iter(self)
-        word, link = next(wotd_generator)
-        lines = ["The word of the day is: {} ({})".format(word, link)]
+        word, speech_part, link, fun_fact = next(wotd_generator)
+        lines = ["The word of the day is: {} ({}): {}".format(word, speech_part, link)]
         for index, definition in enumerate(wotd_generator, 1):
             lines.append("{}. {}".format(index, definition.replace("\n", "\n\n")))
 
+        lines.append(fun_fact)
         return "\n\n".join(lines)
 
 
